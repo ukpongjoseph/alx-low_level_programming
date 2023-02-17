6862c493f870bab9025109b538b0aebca4bee02f
ukpongjoseph <jukpong499@gmail.com>
2023-02-17 13:13:49 -0800

    advanced

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - this is the main function
+ *
+ * Return: always 1
+ */
+int main(void)
+{
+	write("and that piece of art is useful- Dora Korpar, 2015-10-19\n");
+	return (1);
+}
