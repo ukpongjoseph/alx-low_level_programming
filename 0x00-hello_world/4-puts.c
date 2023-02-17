ba51d8d9fa5e6dccd46a64f072bab34a24d03a4b
ukpongjoseph <jukpong499@gmail.com>
2023-02-17 08:16:35 -0800

    betty

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,13 @@
 #include<stdio.h>
+
 /**
- * main - Entry point
- * Return: Always 0 (Success)
+ * main - this is the main function
+ *
+ * Return: always 0
  */
+
 int main(void)
 {
-	put("\"Programming is like building a multilingual puzzle\n");
+	puts("\"Programming is like building a multilingual puzzle);
 	return (0);
 }
