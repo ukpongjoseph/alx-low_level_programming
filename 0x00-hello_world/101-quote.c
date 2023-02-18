d2f222d7268f3945c61f5ced0b222be2532b3934
ukpongjoseph <jukpong499@gmail.com>
2023-02-18 00:11:42 -0800

    qote

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,4 +1,4 @@
-#include <stdio.h>
+#include <unistd.h>
 
 /**
  * main - this is the main function
@@ -6,7 +6,8 @@
  * Return: always 1
  */
 int main(void)
+
 {
-	write("and that piece of art is useful- Dora Korpar, 2015-10-19\n");
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 	return (1);
 }
