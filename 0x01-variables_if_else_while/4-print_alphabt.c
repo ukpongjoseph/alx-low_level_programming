8def819075aa41bedd5194030362eb4f8ab7409f
ukpongjoseph <jukpong499@gmail.com>
2023-02-19 09:23:17 -0800

    except

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - Print and ommit certain alphabets
+ * Return: Always 0 (Success)
+ *
+ */
+int main(void)
+{
+	char low, e, q;
+
+	e = 'e';
+	q = 'q';
+
+	for (low = 'a'; low <= 'z'; low++)
+	{
+	if (low != e && low != q)
+	putchar(low);
+	}
+	putchar('\n');
+	return (0);
+}
