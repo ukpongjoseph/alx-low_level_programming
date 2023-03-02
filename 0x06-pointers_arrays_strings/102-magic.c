7eff999f098d2bcc8cb0993c88c061c01e858f44
ukpongjoseph <jukpong499@gmail.com>
2023-03-02 04:17:59 -0800

    magic

--- 0x06-pointers_arrays_strings/102-magic.c
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -1,10 +1,5 @@
 #include <stdio.h>
 
-/**
- * main - Check the  code.
- * Return: Always 0.
- */
-
 int main(void)
 {
 	int n;
@@ -24,5 +19,9 @@ int main(void)
 *(p + 5) = 98;
 /* ...so that this prints 98\n */
 printf("a[2] = %d\n", a[2];
+/**
+ * main - check the code
+ * Return: Always 0.
+ */
 return (0);
 }
