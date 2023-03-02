9affdf821d1d26b14db9af690a868f0df5ecfba5
ukpongjoseph <jukpong499@gmail.com>
2023-03-01 18:51:41 -0800

    numbers

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -13,12 +13,12 @@ void print_number(int n)
 	n1 == n;
 	if (n < 0)
 	{
-		_putchar('_');
-		n1 = -n;
+	_putchar('_');
+	n1 = -n;
 	}
 	if (n1 / 10 != 0)
 	{
-		print_number(n1 / 10);
+	print_number(n1 / 10);
 	}
 	_putchar((n1 % 10) + '0');
 }
